e3df6095556f8b31c058595283bf4abf678065da
Denis Pessoa <dnspessoa1@gmail.com>
2021-05-20 09:05:24 -0300

    Adicionar arquivos de projeto.

---
+++ ControleFinanceiro.API/Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Permite compatilhar recursos frontend e o backend
+            services.AddCors();
+
+            //Conf localização arquivos do angular
+            services.AddSpaStaticFiles(dir => {
+                dir.RootPath = "ControleFinanceiro-UI";
+            });
+
+            services.AddControllers()
+                .AddJsonOptions(opc => {
+                    opc.JsonSerializerOptions.IgnoreNullValues = true;
+                })
+                .AddNewtonsoftJson(opc => {
+                    opc.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //Permite compatilhar recursos frontend e o backend
+            app.UseCors(opc => opc.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //Conf localização arquivos do angular
+            app.UseStaticFiles();
+            //Conf localização arquivos do angular
+            app.UseSpaStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            //Conf localização arquivos do angular
+            app.UseSpa(spa => {
+                spa.Options.SourcePath = Path.Combine(Directory.GetCurrentDirectory(), "ControleFinanceiro-UI");
+
+                //conf Proxy
+                if (env.IsDevelopment())
+                {
+                    spa.UseProxyToSpaDevelopmentServer($"http://localhost:4200/");
+                }
+            });
+
+        }
+    }
+}
